eb2b0c8aa7032de89edc67f2f657749f32ef6c3c
nmlgc <nmlgc@nmlgc.net>
2023-11-08 23:21:47 +0100

    [CLI] Start the project
    Trying out these 100-character lines for a change, which is rustfmt's
    default anyway.
    
    Part of P0266, funded by Ember2528.

---
+++ README.md
@@ -0,0 +1,5 @@
+# mly
+
+![License](https://img.shields.io/github/license/nmlgc/mly?cacheSeconds=31536000)
+
+Unix-style filter suite for Standard MIDI Files, built on top of the [midly crate](https://crates.io/crates/midly).
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Cli {}
+
+fn run(_: Cli) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run(Cli::parse()) {
+        let args = std::env::args()
+            .skip(1)
+            .fold(String::new(), |a, b| a + " " + &b);
+        eprintln!(
+            "`{}{args}`: error: {e}",
+            std::env::current_exe()
+                .ok()
+                .as_ref()
+                .and_then(|p| p.file_stem())
+                .map(|p| p.to_string_lossy())
+                .unwrap_or(env!("CARGO_PKG_NAME").into())
+        );
+        std::process::exit(1);
+    }
+}
