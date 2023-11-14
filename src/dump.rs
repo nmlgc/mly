698740315fa9bd79d6e813528cbceeb62043256e
nmlgc <nmlgc@nmlgc.net>
2023-11-14 01:39:35 +0100

    [Dump] Output byte parameters as hex strings
    …OK, still need to investigate why 6 of the 19 files in the arranged
    soundtrack ignore the volume setting in Sound Canvas VA. Hex strings
    have a consistent horizontal width, and are easier to cross-reference
    with hardware manuals.
    We probably will want to print strings as raw bytes later, but
    std::fmt's forced UTF-8 encoding makes this harder than it needs to be.
    
    Part of P0266, funded by Ember2528.

--- src/dump.rs
+++ src/dump.rs
@@ -1,10 +1,48 @@
 //! MIDI event dumping.
 
-use std::cmp::max;
+use std::{cmp::max, fmt::Debug};
 
-use midly::Smf;
+use midly::{MetaMessage, Smf, TrackEventKind};
 
 use crate::time::MidiTimeDisplay;
+
+struct Hex<'a>(&'a [u8]);
+
+impl<'a> std::fmt::Display for Hex<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, byte) in self.0.iter().enumerate() {
+            write!(f, "{}{:02X}", if i != 0 { " " } else { "" }, byte)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct KindDisplay<'a>(&'a TrackEventKind<'a>);
+
+impl<'a> std::fmt::Display for KindDisplay<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            TrackEventKind::SysEx(b) => write!(f, "SysEx({})", Hex(b)),
+            TrackEventKind::Meta(meta) => match meta {
+                MetaMessage::Text(b) => write!(f, "Meta(Text({}))", Hex(b)),
+                MetaMessage::Copyright(b) => write!(f, "Meta(Copyright({}))", Hex(b)),
+                MetaMessage::TrackName(b) => write!(f, "Meta(TrackName({}))", Hex(b)),
+                MetaMessage::Marker(b) => write!(f, "Meta(Marker({}))", Hex(b)),
+                MetaMessage::InstrumentName(b) => write!(f, "Meta(InstrumentName({}))", Hex(b)),
+                MetaMessage::Lyric(b) => write!(f, "Meta(Lyric({}))", Hex(b)),
+                MetaMessage::CuePoint(b) => write!(f, "Meta(CuePoint({}))", Hex(b)),
+                MetaMessage::ProgramName(b) => write!(f, "Meta(ProgramName({}))", Hex(b)),
+                MetaMessage::DeviceName(b) => write!(f, "Meta(DeviceName({}))", Hex(b)),
+                MetaMessage::SequencerSpecific(b) => {
+                    write!(f, "Meta(SequencerSpecific({}))", Hex(b))
+                }
+                _ => self.0.fmt(f),
+            },
+            _ => self.0.fmt(f),
+        }
+    }
+}
 
 pub fn dump(smf: &Smf) {
     let delta_header = "Delta";
@@ -28,11 +66,11 @@ pub fn dump(smf: &Smf) {
         for ev in track {
             time.time = time.time + ev;
             println!(
-                "{:>+delta_width$}\t{:>pulse_width$}\t{:>beat_width$}\t{:?}",
+                "{:>+delta_width$}\t{:>pulse_width$}\t{:>beat_width$}\t{:}",
                 ev.delta,
                 time.display_pulse(),
                 time.display_beat(),
-                ev.kind,
+                KindDisplay(&ev.kind),
             );
         }
     }
